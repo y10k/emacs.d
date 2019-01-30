ee80e77fbe5ddce90c97bd570876a11fc72eb676
TOKI Yoshinori <toki@freedom.ne.jp>
2019-01-30 19:30:14 +0900

    custom-set-faces: region
    for xterm-256color.
    not to make the color the same as the face of font-lock-function-name-face.

--- init.el
+++ init.el
@@ -570,7 +570,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "DejaVu Sans Mono")))))
+ '(default ((t (:inherit nil :stipple nil :background "Black" :foreground "White" :inverse-video nil :box nil :strike-through nil :overline nil :underline nil :slant normal :weight normal :height 113 :width normal :foundry "unknown" :family "DejaVu Sans Mono"))))
+ '(region ((t (:background "blue3" :foreground "brightwhite")))))
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
