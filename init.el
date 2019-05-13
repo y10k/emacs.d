0c31436fb139ea52e2bed9a0fd8ddb14fae8e49b
TOKI Yoshinori <toki@freedom.ne.jp>
2019-05-13 16:20:00 +0900

    update package-archives

--- init.el
+++ init.el
@@ -54,8 +54,9 @@
 
 ;; Emacs Lisp Package Archive
 (require 'package)
-(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/"))
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/") t)
 (package-initialize)
 
 ;; Japanese environment
