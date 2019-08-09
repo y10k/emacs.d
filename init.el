9371e644f957a15bd07e697f26d7fd17c4266fec
TOKI Yoshinori <toki@freedom.ne.jp>
2019-08-10 00:36:21 +0900

    fixed-pitch face: disabled font family
    Because default monospace font will be available.

--- init.el
+++ init.el
@@ -630,5 +630,11 @@ ARGS is original arguments."
 (put 'downcase-region 'disabled nil)
 (put 'narrow-to-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(fixed-pitch ((t nil))))
 
 ;;; init.el ends here
