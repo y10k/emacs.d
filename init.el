58ce47aa27b351d0dcd21ed2f094b392d6bb93e2
TOKI Yoshinori <toki@freedom.ne.jp>
2019-08-23 23:04:21 +0900

    ruby-test-unit.el: for ruby-japanize

--- init.el
+++ init.el
@@ -294,7 +294,7 @@
 (setq ruby-test-unit-runner-options "--no-use-color") ; for test-unit on ruby-2.2.0 or later.
 (defconst ruby-test-unit-test-code-regexp-list
   (append ruby-test-unit-test-code-regexp-list
-          '("^\\s *\\(require\\|必要\\).*\\s\"japanize/test_unit\\s\"")))
+          '("^\\s *\\(require\\|要求\\).*\\s\"japanize/test_unit\\s\"")))
 (setq ruby-program-name
       (concat "ruby " (expand-file-name "/usr/local/bin/irb") " --inf-ruby-mode"))
 (setq yari-ri-program-name "bundle exec ri")
